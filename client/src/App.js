7bd02d22b9144471bc08b3791d420864c12aec5b
Phillip <phillipjferreira@gmail.com>
2020-07-02 13:21:21 -0400

    Initialize app

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>PHILLIP FERREIRA - SOFTWARE ENGINEER</p>
+        <a
+          className="App-link"
+          href="http://www.logger.phillipferreira.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Logger
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
